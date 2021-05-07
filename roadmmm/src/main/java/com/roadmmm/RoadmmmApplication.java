ae1f2574614f625746fb0e463716d086e4f33819
kkj53051000 <tkarnrwl226@gmail.com>
2021-05-07 18:20:12 +0900

    ddadada

--- roadmmm/src/main/java/com/roadmmm/RoadmmmApplication.java
+++ roadmmm/src/main/java/com/roadmmm/RoadmmmApplication.java
@@ -2,7 +2,9 @@ package com.roadmmm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.roadmmm"})
 @SpringBootApplication
 public class RoadmmmApplication {
 
